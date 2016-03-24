84d76d6a14ae4465532981bcc228612f80c21a61
studentzero <flipwinters@gmail.com>
2016-03-24 16:23:16 +0100

    seventh commit

--- public/post/index.php
+++ public/post/index.php
@@ -4,8 +4,10 @@ session_start()
 
 
 <form action="" method="post">
-    Name: <input type="text" name="name"><br>
-    Email: <input type="email" name="mail">
+    Naam: <input type="text" name="name"><br>
+    Email: <input type="email" name="mail"><br>
+    <input type="radio" name="gender" value="male">Man<br>
+    <input type="radio" name="gender" value="female"> Vrouw
     <input type="submit" name="submit">
 </form>
 
@@ -15,10 +17,11 @@ session_start()
 if (isset($_POST['submit'])) {
 $_SESSION['name'] = $_POST['name'];
 $_SESSION['mail'] = $_POST['mail'];
+$_SESSION['gender'] = $_POST['gender'];
 }
 
 ?>
 
-<strong><?php echo $_SESSION['name'], $_SESSION['mail'];?></strong>
+<strong><?php echo $_SESSION['name'], "<br />", $_SESSION['mail'], "<br />", $_SESSION['gender'];?></strong>
 
 
